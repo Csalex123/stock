3353b4f3d9459b36395c086b964cca23cf4d3343
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-18 23:26:51 -0300

    Reset css and setup project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import TestComponent from '../TestComponent';
-// import ClassComponent from '../ClassComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <TestComponent name="Isso é uma props" />
-      {/* <ClassComponent name="Classe" /> */}
+        ul
     </div>
   );
 }
