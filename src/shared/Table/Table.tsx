2440bef3564d0752014264ac0513c059701fcf54
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-19 15:08:21 -0300

    Add table html structure

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -8,7 +8,21 @@ declare interface TableProps {
 const Table: React.FC<TableProps> = (props) => {
     return (
         <table>
-            
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Teste</td>
+                    <td>teste2</td>
+                    <td>teste3</td>
+                </tr>
+            </tbody>
         </table>
     );
 }
