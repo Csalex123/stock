f288ffc66d50232e16fd9e7bd3f48b2b4cab948a
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-19 14:41:09 -0300

    Add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../Header';
 
 import '../../global.css';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 
 // function TestComponent() {
 //   return <div>Componente de teste.</div>
 // }
 
 function App() {
+  const [street, setStreet] = useState('');
+
   return (
     <div className="App">
       <Header title="Stock" />
@@ -22,6 +25,12 @@ function App() {
           onClick={() => window.alert('teste')}>
           Botão 1
         </Button>
+        <Input
+          label="Street"
+          placeholder="Ex.: 15h Avenue"
+          value={street}
+          onChange={e => setStreet(e.target.value)}
+        />
       </Container>
 
     </div>
---
+++ src/shared/Input/Input.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Input.css';
+
+declare interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
+    label?: string
+}
+
+const Input: React.FC<InputProps> = (props) => {
+    return (
+        <div className="AppInput">
+            <label htmlFor="">
+                <span>{props.label}</span>
+                <input
+                    {...props}
+                />
+            </label>
+        </div>
+    );
+};
+
+export default Input;
