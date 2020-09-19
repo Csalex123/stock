7fc0ef382ace841960b27355d2018557f3c0da97
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-18 23:08:49 -0300

    Component DidMount and DidUpdate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TestComponent from '../TestComponent';
-import ClassComponent from '../ClassComponent';
+// import ClassComponent from '../ClassComponent';
 
 function App() {
   return (
     <div className="App">
       <TestComponent name="Isso é uma props" />
-      <ClassComponent name="Classe" />
+      {/* <ClassComponent name="Classe" /> */}
     </div>
   );
 }
--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 const TextComponent = (props: { name: string }) => {
     const [age, setAge] = useState(21)
-    // const [a, setA] = useState(0);
-    // const [b, setB] = useState(0);
+
+    useEffect(() => {
+        console.log('Component was created');
+    }, []);
+
+    useEffect(() => {
+        console.log('Component alterado');
+    }, [age]);
 
     return (
         <div className="container">
             <h1>{age}</h1>
-            {/* <input type="text" placeholder="a" value={a} onChange={e => setA(e.target.value)} />
-            <input type="text" placeholder="b" value={b} onChange={e => setB(e.target.value)}/> */}
             <button onClick={(e) => setAge(age + 2)}>Button</button>
         </div>
     )
